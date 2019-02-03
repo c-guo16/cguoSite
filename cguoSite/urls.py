1b9287a20ed40a58c61427f151ee0156429ab99c
cguo16 <513245873@qq.com>
2019-02-03 19:06:18 +0800

    first test passed.

--- cguoSite/urls.py
+++ cguoSite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainPage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'',views.index),
+
 ]
