e544433f6b2b916bd29d745e5dd90ef89bd4462e
cguo16 <513245873@qq.com>
2019-02-04 13:57:25 +0800

    add visit times.

--- mainPage/views.py
+++ mainPage/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 
+#访问次数：
+visit_times=0
+
 def index(request):
-    return render(request,'index.html')
+    global visit_times
+    visit_times = visit_times + 1
+    return render(request,'index.html',{"v_times":visit_times})
