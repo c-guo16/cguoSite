d6cd391e1126961d846e6c64e27e1a3e24df8537
cguo16 <513245873@qq.com>
2019-02-07 23:01:40 +0800

    template practice

--- mainPage/views.py
+++ mainPage/views.py
@@ -6,7 +6,18 @@ from django.shortcuts import HttpResponse
 #访问次数：
 visit_times=0
 
+#获取用户ip：
+def get_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]#所以这里是真实的ip
+    else:
+        ip = request.META.get('REMOTE_ADDR')#这里获得代理ip
+    return ip
+
+#首页：
 def index(request):
     global visit_times
-    visit_times = visit_times + 1
+    visit_times = visit_times + 1  #访问次数增加
+    print(get_ip(request))          #打印ip
     return render(request,'index.html',{"v_times":visit_times})
