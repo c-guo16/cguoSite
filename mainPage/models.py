acec00a3d0267f3592f5dd7d88246012f8087547
cguo16 <513245873@qq.com>
2019-02-24 15:04:11 +0800

    2019.2.24

--- mainPage/models.py
+++ mainPage/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     username=models.CharField(max_length=20)
-    password=models.CharField(max_length=16)
+    password=models.CharField(max_length=40)
     mail = models.EmailField(max_length=50)
     isActive=models.BooleanField(default=False)
     uuid=models.CharField(max_length=50)
